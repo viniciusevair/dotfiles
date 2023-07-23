5dfc408006a686105cc2aaa3abdb38ba44593241
Vinicius Evair <vsilva.evair@gmail.com>
2023-07-23 11:28:08 -0300

    Enabled autosnippets

--- nvim/after/plugin/snippets.lua
+++ nvim/after/plugin/snippets.lua
@@ -26,5 +26,5 @@ local parse = require("luasnip.util.parser").parse_snippet
 local ms = ls.multi_snippet
 local k = require("luasnip.nodes.key_indexer").new_key
 
--- ls.config.setup({ enable_autosnippets = true })  PERIGOSO, MAS MANEIRO!!
+ls.config.setup({ enable_autosnippets = true })
 require("luasnip.loaders.from_lua").lazy_load { paths = "~/.config/nvim/after/snippets/" }
