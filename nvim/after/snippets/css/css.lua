b61ddfdb81442be92a5bd259df34d4422cef8945
Vinicius Evair <vsilva.evair@gmail.com>
2023-07-23 11:30:12 -0300

    Started webdev snippets folders

---
+++ nvim/after/snippets/css/css.lua
@@ -0,0 +1,9 @@
+return {
+    s({
+        trig = "sst",
+        dscr = "CSS code starter, with margin and padding resets",
+    },
+    {
+        t({"* {", "\tbox-sizing: border-box;", "\tmargin: 0;", "\tpadding: 0;"}),
+    }),
+}
