c05c52ff516896f1e0efd12318754bacfe7321d0
Vinicius Evair <vsilva.evair@gmail.com>
2023-07-23 11:33:30 -0300

    Refactored whole organization, also added some webdev/QoL plugins

--- nvim/lua/evair/packer.lua
+++ nvim/lua/evair/packer.lua
@@ -8,20 +8,30 @@ return require('packer').startup(function(use)
     use 'junegunn/seoul256.vim'
 
     -- Plugins --
+
+    -- Graphic plugins
+    -- HUD
+    use 'nvim-lualine/lualine.nvim'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use {
+        "rrethy/vim-hexokinase",
+        run = "make",
+    }
+    -- Treesitter
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use 'nvim-treesitter/nvim-treesitter-context'
+    -- Colorpicker
+    use 'uga-rosa/ccc.nvim'
 
+    -- Navigation
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-
-    use 'nvim-lualine/lualine.nvim'
-    use 'nvim-tree/nvim-web-devicons'
-    use 'lukas-reineke/indent-blankline.nvim'
-
     use {"akinsho/toggleterm.nvim", tag = '*'}
 
+    -- Language Server Provider
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
@@ -29,7 +39,6 @@ return require('packer').startup(function(use)
             {'neovim/nvim-lspconfig'},
             {'williamboman/mason.nvim'},
             {'williamboman/mason-lspconfig.nvim'},
-
             -- Autocompletion
             {'hrsh7th/nvim-cmp'},
             {'onsails/lspkind.nvim'},
@@ -38,11 +47,19 @@ return require('packer').startup(function(use)
             {'saadparwaiz1/cmp_luasnip'},
             {'hrsh7th/cmp-nvim-lsp'},
             {'hrsh7th/cmp-nvim-lua'},
-
             -- Snippets
             {'L3MON4D3/LuaSnip'},
         }
     }
 
+    -- LaTeX
     use 'lervag/vimtex'
+
+    -- Formatting
+    use 'prettier/vim-prettier'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-commentary'
+
+    -- Git integration
+    use 'tpope/vim-fugitive'
 end)
