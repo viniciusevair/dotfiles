60578b748ccda6af5450e934a3bd6c215a0a0878
Vinicius Evair <vsilva.evair@gmail.com>
2023-07-25 11:42:06 -0300

    Added python snippets directory

---
+++ nvim/after/snippets/python/python.lua
@@ -0,0 +1,9 @@
+return {
+    s({
+        trig = "sst",
+        dscr = "Starter code, just the shebang, basically",
+    },
+    {
+        t({"#!/usr/bin/env python3.11", "", ""}), i(0),
+    }),
+}
