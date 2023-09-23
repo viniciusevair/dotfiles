4bc03b728ce0dd77debaed91aec3d627543a3740
Vinicius Evair <vsilva.evair@gmail.com>
2023-09-22 21:27:32 -0300

    Added some config to vimtex

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -328,6 +328,24 @@ require("lazy").setup({
     {
         "lervag/vimtex",
         ft = "tex",
+        config = function()
+            vim.g.vimtex_view_method = 'zathura'
+            vim.g.vimtex_quickfix_ignore_filters = {
+                "Command terminated with space",
+                "LaTeX Font Warning: Font shape",
+                "Package caption Warning: The option",
+                [[Underfull \\hbox (badness [0-9]*) in]],
+                "Package enumitem Warning: Negative labelwidth",
+                [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in]],
+                [[Package caption Warning: Unused \\captionsetup]],
+                "Package typearea Warning: Bad type area settings!",
+                [[Package fancyhdr Warning: \\headheight is too small]],
+                [[Underfull \\hbox (badness [0-9]*) in paragraph at lines]],
+                "Package hyperref Warning: Token not allowed in a PDF string",
+                [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
+            }
+            vim.g.vimtex_quickfix_mode=0
+        end,
     },
     -- }}}
 
