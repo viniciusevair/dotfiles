7748474622d94af261f928ed5b89e1f16b0ac578
Vinicius Evair da Silva <Vinicius.Silva@ipinfusion.com>
2024-05-06 14:32:47 -0300

    Deleted old version of zsh and nvim directories

--- nvim/after/snippets/css/css.lua
+++
@@ -1,9 +0,0 @@
-return {
-    s({
-        trig = "sst",
-        dscr = "CSS code starter, with margin and padding resets",
-    },
-    {
-        t({"* {", "\tbox-sizing: border-box;", "\tmargin: 0;", "\tpadding: 0;"}),
-    }),
-}
--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- Lazy is weird, had to move some stuff out here
-vim.opt.termguicolors = true
-require("remap")
-require("plugins")
-require("config")
