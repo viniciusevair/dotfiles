391e27c1a0fd15f1ea66b2cdf467d3b3b2dfacdd
Vinicius Evair <vsilva.evair@gmail.com>
2025-01-06 17:36:00 -0300

    Remade system monitor from scratch
    TODO: update values. currently its just a static bar

--- awesome/layout/panel/calendar/sys.lua
+++ awesome/layout/panel/calendar/sys.lua
@@ -1,54 +1,58 @@
-local awful         = require "awful"
-local wibox         = require "wibox"
-local beautiful     = require('beautiful')
+local beautiful = require ("beautiful")
+local colors = require ("themes.colors")
+local helpers = require ("helpers")
+local wibox = require ("wibox")
+local dpi = beautiful.xresources.apply_dpi
 
-local dpi           = beautiful.xresources.apply_dpi
-local helpers       = require('helpers')
-local color         = require("themes.colors")
+-- Function to create a progress bar with label
+local function create_progress_widget (name, color)
+	-- Create the progress bar
+	local progress_bar = wibox.widget ({
+		max_value = 100,
+		value = 0,
+		widget = wibox.widget.progressbar,
+		forced_width = 250,
+		forced_height = 20,
+		border_color = colors.magenta,
+		border_width = dpi (2),
+		background_color = colors.bg_light,
+		color = color,
+	})
 
-local create_slider = function(name, max, current, color1, color2)
-	local progress = {
-		id               = "pro",
-		max_value        = max,
-		value            = current,
-		forced_height    = 20,
-		forced_width     = 250,
-		bar_shape        = helpers.rrect(0),
-		shape            = helpers.rrect(0),
-		color            = color1,
-		background_color = color.bg_light,
-		paddings         = 1,
-		border_width     = 1,
-		widget           = wibox.widget.progressbar,
-	}
+	-- Label widget
+	local label = helpers.textbox (colors.magenta, "IosevkaTermNF bold 16", name)
+	label.forced_width = dpi (50)
 
-	local label =
-			helpers.margin(helpers.textbox(color2, "IosevkaTermNF bold 16", name), 15, 6, 0, 0)
+	-- Final layout for the progress bar and label
+	local final = wibox.widget ({
+		helpers.margin (label, 15, 6, 0, 0),
+		helpers.margin (progress_bar, 6, 15, 0, 0),
+		layout = wibox.layout.align.horizontal,
+	})
 
-	label.forced_width = dpi(80)
-
-	local final = wibox.widget {
-		label,
-		helpers.margin(progress, 6, 15, 0, 0),
-		layout = wibox.layout.align.horizontal
-	}
-
-	return helpers.margin(final, 3, 3, 15, 15)
+	return helpers.margin (final, 3, 3, 15, 15), progress_bar
 end
 
-local sys           = wibox.widget {
+-- Create all progress bars and their labels
+local cpu_widget, cpu_controller = create_progress_widget ("CPU", colors.orange)
+local ram_widget, ram_controller = create_progress_widget ("RAM", colors.green)
+local disk_widget, disk_controller =
+	create_progress_widget ("DISK", colors.purple)
+
+local sys = wibox.widget ({
 	{
 		{
-			create_slider('CPU', 100, 6, color.orange, color.red),
-			create_slider('RAM', 100, 25, color.green, color.red),
-			create_slider('DISK', 100, 69, color.purple, color.red),
-			layout = wibox.layout.fixed.vertical
+			cpu_widget,
+			ram_widget,
+			disk_widget,
+			layout = wibox.layout.fixed.vertical,
 		},
 		widget = wibox.container.margin,
-		margins = dpi(10)
+		margins = dpi (10),
 	},
 	widget = wibox.container.background,
-	bg = color.bg_normal,
-	shape = helpers.rrect(0) }
+	bg = colors.bg_normal,
+	shape = helpers.rrect (0),
+})
 
-return helpers.margin(sys, 10, 10, 10, 10)
+return helpers.margin (sys, 10, 10, 10, 10)
