ed74a1add877e221cb17d46bbe27d6550908dbf2
Vinicius Evair <vsilva.evair@gmail.com>
2025-01-06 21:16:37 -0300

    It works now :)

--- awesome/layout/panel/calendar/sys.lua
+++ awesome/layout/panel/calendar/sys.lua
@@ -2,6 +2,8 @@ local beautiful = require ("beautiful")
 local colors = require ("themes.colors")
 local helpers = require ("helpers")
 local wibox = require ("wibox")
+local awful = require ("awful")
+local gears = require ("gears")
 local dpi = beautiful.xresources.apply_dpi
 
 -- Function to create a progress bar with label
@@ -33,11 +35,44 @@ local function create_progress_widget (name, color)
 	return helpers.margin (final, 3, 3, 15, 15), progress_bar
 end
 
--- Create all progress bars and their labels
-local cpu_widget, cpu_controller = create_progress_widget ("CPU", colors.orange)
-local ram_widget, ram_controller = create_progress_widget ("RAM", colors.green)
-local disk_widget, disk_controller =
-	create_progress_widget ("DISK", colors.purple)
+local cpu_widget, cpu_controller = create_progress_widget("CPU", colors.orange)
+local ram_widget, ram_controller = create_progress_widget("RAM", colors.green)
+local disk_widget, disk_controller = create_progress_widget("DISK", colors.purple)
+
+local cpu_widget, cpu_controller = create_progress_widget("CPU", colors.orange)
+local ram_widget, ram_controller = create_progress_widget("RAM", colors.green)
+local disk_widget, disk_controller = create_progress_widget("DISK", colors.purple)
+
+local function update_controller()
+  awful.spawn.easy_async_with_shell(
+    "echo \"$(top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8}') $(free | grep Mem | awk '{printf \"%.2f\", $3/$2 * 100.0}') $(df -h / | awk 'NR==2 {print $5}' | tr -d '%')\"",
+    function(stdout)
+      -- Split the stdout into CPU, RAM, and Disk values
+      local cpu_usage, ram_usage, disk_usage = stdout:match("([%d%.]+) ([%d%.]+) ([%d]+)")
+
+      -- Update each controller
+      if cpu_usage then
+        cpu_controller:set_value (tonumber(cpu_usage))
+      end
+      if ram_usage then
+        ram_controller:set_value (tonumber(ram_usage))
+      end
+      if disk_usage then
+        disk_controller:set_value (tonumber(disk_usage))
+      end
+    end
+  )
+end
+
+-- Timer to periodically update widgets
+gears.timer {
+  timeout = 5,
+  autostart = true,
+  call_now = true,
+  callback = function()
+    update_controller()
+  end
+}
 
 local sys = wibox.widget ({
 	{
