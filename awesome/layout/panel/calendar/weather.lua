a5b392b65b230a7db4d71a42e7bd4e6315a5a2df
Vinicius Evair <vsilva.evair@gmail.com>
2025-01-07 15:02:03 -0300

    Started implementation of functional weather widget

--- awesome/layout/panel/calendar/weather.lua
+++ awesome/layout/panel/calendar/weather.lua
@@ -1,32 +1,83 @@
-local awful          = require 'awful'
-local wibox          = require('wibox')
-local beautiful      = require('beautiful')
+local awful        = require 'awful'
+local gears        = require 'gears'
+local wibox        = require('wibox')
+local beautiful    = require('beautiful')
 
-local dpi            = beautiful.xresources.apply_dpi
-local helpers        = require('helpers')
-local color          = require("themes.colors")
+local dpi          = beautiful.xresources.apply_dpi
+local helpers      = require('helpers')
+local color        = require("themes.colors")
 
-local user           = require('user')
-local iconpath       = os.getenv("HOME") .. '/.config/awesome/assets/weather_icons/'
+local user         = require('user')
+local iconpath     = os.getenv("HOME") .. '/.config/awesome/assets/weather_icons/'
+
+local api_key      = "e3578ff20a78750dbfc8f2e9ece0c0d1"
+local city         = "Curitiba"
+local country_code = "BR"
+local weather_api  = "https://api.openweathermap.org/data/2.5/weather?q=" ..
+                     city .. "," .. country_code .. "&appid=" .. api_key ..
+                     "&units=metric"
 
 --------------------------
 --Base_widgets------------
 --------------------------
-local city_text      = helpers.textbox(color.lightblue, "IosevkaTermNF bold 12", " Earth (probably)")
-local country_text   = helpers.textbox(color.fg_normal, "IosevkaTermNF bold 12", ",SS")
-local weather_txt    = helpers.textbox(color.lightblue, "IosevkaTermNF bold 12", "Loading...")
-local temp_txt       = helpers.textbox(color.fg_normal, "IosevkaTermNF bold 30", " " .. '°C')
+local icon_location  = "  "
+local text_location  = helpers.textbox(color.lightblue, "IosevkaTermNF bold 20",
+                       "Curitiba, BR")
+local text_weather   = helpers.textbox(color.lightblue, "IosevkaTermNF bold 12", "Loading...")
+local text_temp      = helpers.textbox(color.fg_normal, "IosevkaTermNF bold 30", " " .. '°C')
 
 local icon_humid     = helpers.textbox(color.blue, "IosevkaTermNF bold 17", "󱪅 ")
 local icon_wind      = helpers.textbox(color.blue, "IosevkaTermNF bold 17", " ")
-local icon_feelslike = helpers.textbox(color.blue, "IosevkaTermNF bold 17", "󱤋 ")
+local icon_feelslike = helpers.textbox(color.blue, "IosevkaTermNF bold 17", "󱤋  ")
 
-local NA             = helpers.textbox(color.fg_normal, "IosevkaTermNF 14", "N/A")
+local text_humid     = helpers.textbox(color.fg_normal, "IosevkaTermNF 14", "N/A")
+local text_wind      = helpers.textbox(color.fg_normal, "IosevkaTermNF 14", "N/A")
+local text_feelslike = helpers.textbox(color.fg_normal, "IosevkaTermNF 14", "N/A")
 
 local weath_image    = helpers.imagebox(iconpath .. 'weather-error.svg', 70, 70)
 
+local function update_weather()
+  awful.spawn.easy_async_with_shell(
+    "curl -s '" .. weather_api .. "'",
+    function(stdout)
+      -- Parse JSON response
+      local json = require("dkjson") -- Requires LuaJSON module
+      local weather_data, pos, err = json.decode(stdout, 1, nil)
+
+      if err then
+        text_weather:set_text("Error fetching weather")
+        return
+      end
+
+      -- Update widget with data
+      local city_name = weather_data.name
+      local country = weather_data.sys.country
+      local weather_main = weather_data.weather[1].description
+      local temp = math.floor(weather_data.main.temp)
+      local feels_like = math.floor(weather_data.main.feels_like)
+      local humidity = weather_data.main.humidity
+      local wind_speed = weather_data.wind.speed
+
+      -- Update the widget components
+      text_location:set_text(icon_location .. city_name .. ", " .. country)
+      text_weather:set_text(weather_main)
+      text_temp:set_text(temp .. "°C")
+      text_humid:set_text(humidity .. "%")
+      text_feelslike:set_text(feels_like .. "°C")
+      text_wind:set_text(wind_speed .. " m/s")
+    end
+  )
+end
+
+gears.timer {
+  timeout = 30,
+  call_now = true,
+  autostart = true,
+  callback = update_weather
+}
+
 --Bottom containers
-local container      = function(wgt1, wgt2)
+local container      = function(wgt1, wgt2, width)
 	return wibox.widget {
 		{
 			{
@@ -37,7 +88,7 @@ local container      = function(wgt1, wgt2)
 					layout = wibox.layout.align.horizontal
 				},
 				widget       = wibox.container.margin,
-				forced_width = dpi(110),
+				forced_width = dpi(width),
 				left         = dpi(10),
 				right        = dpi(10),
 				top          = dpi(5),
@@ -51,21 +102,19 @@ local container      = function(wgt1, wgt2)
 		margins = dpi(0) }
 end
 
-
-
 local wgt = wibox.widget {
 	{
 		{
 			{
 				helpers.margin({
-						city_text,
+            text_location,
 						nil,
-						weather_txt,
+						text_weather,
 						layout = wibox.layout.align.horizontal
 					},
 					5, 5, 10, 10),
 				helpers.margin({
-						temp_txt,
+						text_temp,
 						nil,
 						weath_image,
 						layout = wibox.layout.align.horizontal
@@ -73,10 +122,10 @@ local wgt = wibox.widget {
 					10, 5, 5, 10),
 				helpers.margin({
 						{
-							helpers.margin(container(icon_humid, NA), 5, 12, 0, 0),
-							helpers.margin(container(icon_feelslike, NA), 12, 12, 0, 0),
-							helpers.margin(container(icon_wind, NA), 12, 5, 0, 0),
-							layout = wibox.layout.fixed.horizontal
+							helpers.margin(container(icon_humid, text_humid, 115), 5, 12, 0, 0),
+							helpers.margin(container(icon_feelslike, text_feelslike, 115), 12, 12, 0, 0),
+							helpers.margin(container(icon_wind, text_wind, 120), 12, 5, 0, 0),
+							layout = wibox.layout.align.horizontal
 						},
 						widget = wibox.container.place
 					},
