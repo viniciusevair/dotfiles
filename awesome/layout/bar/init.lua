83f90a67670fa01accd9cf47579d204069613a8f
Vinicius Evair <vsilva.evair@gmail.com>
2025-03-29 15:56:53 -0300

    Started implementation of different screens

--- awesome/layout/bar/init.lua
+++ awesome/layout/bar/init.lua
@@ -58,134 +58,136 @@ awful.screen.connect_for_each_screen(function(s)
 	--Taglist---------
 	local taglist = require("layout.bar.modules.taglist")(s)
 
-	--------------------
-	--Bar---------------
-	--------------------
-	s.bar = awful.wibar {
-		width = dpi(50),
-		screen = s,
-		position = "left",
-		height = awful.screen.focused().workarea.height,
-		bg = "#000000",
-		widget = wibox.container.background,
-		x = dpi(00),
-		shape = helpers.rrect(0)
-	}
+    --------------------
+    --Bar---------------
+    --------------------
+    s.bar = awful.wibar {
+      width = dpi(50),
+      screen = s,
+      position = "left",
+      height = awful.screen.focused().workarea.height,
+      bg = "#000000",
+      widget = wibox.container.background,
+      x = dpi(00),
+      shape = helpers.rrect(0)
+    }
 
-	s.bar:setup({
-		{
-			{
-				{ buttons,   layout = wibox.layout.fixed.vertical },
-				nil,
-				{ systray,  clock, battery_widget,                      layout = wibox.layout.fixed.vertical },
-				layout = wibox.layout.align.vertical
-			},
-			{
-				{
-					taglist,
-					layout = wibox.layout.fixed.vertical
-				},
-				widget = wibox.container.place
-			},
-			sep,
-			layout = wibox.layout.stack
-		},
-		widget = wibox.container.background,
-		bg = color.bg_darker,
-	}
-	)
+    s.bar:setup({
+      {
+        {
+          { buttons,   layout = wibox.layout.fixed.vertical },
+          nil,
+          { systray,  clock, battery_widget,                      layout = wibox.layout.fixed.vertical },
+          layout = wibox.layout.align.vertical
+        },
+        {
+          {
+            taglist,
+            layout = wibox.layout.fixed.vertical
+          },
+          widget = wibox.container.place
+        },
+        sep,
+        layout = wibox.layout.stack
+      },
+      widget = wibox.container.background,
+      bg = color.bg_darker,
+    }
+    )
 
-	-- s.bar:struts({ left = 50 })
+    -- s.bar:struts({ left = 50 })
 
-	--------------------------
-	--Side panel--------------
-	--------------------------
-	s.bar2 = awful.popup {
-		width = dpi(450),
-		screen = s,
-		height = awful.screen.focused().workarea.height,
-		ontop = true,
-		bg = "#000000",
-		widget = wibox.container.background,
-		x = dpi(-450),
-		shape = helpers.rrect(0),
-		visible = true
-	}
+  if s.index == 1 then
+    --------------------------
+    --Side panel--------------
+    --------------------------
+    s.bar2 = awful.popup {
+      width = dpi(450),
+      screen = s,
+      height = awful.screen.focused().workarea.height,
+      ontop = true,
+      bg = "#000000",
+      widget = wibox.container.background,
+      x = dpi(-450),
+      shape = helpers.rrect(0),
+      visible = true
+    }
 
-	s.bar2:setup {
-		{
-			panel,
-			sep2,
-			layout = wibox.layout.stack
-		},
-		widget = wibox.container.background,
-		bg = color.bg_dim
-	}
+    s.bar2:setup {
+      {
+        panel,
+        sep2,
+        layout = wibox.layout.stack
+      },
+      widget = wibox.container.background,
+      bg = color.bg_dim
+    }
 
-	-----------------------
-	--Toggle sidebar-------
-	-----------------------
-	awesome.connect_signal("slide::bar", function()
-		s.bar.ontop = true
-		if s.bar.x == 0 then
-			local timed = rubato.timed {
-				duration = 1 / 6,
-				intro = 1 / 13,
-				override_dt = true,
-				subscribed = function(pos)
-					s.bar.x = pos
-				end
-			}
-			timed.target = 450
-		end
-	end)
+    -----------------------
+    --Toggle sidebar-------
+    -----------------------
+    awesome.connect_signal("slide::bar", function()
+      s.bar.ontop = true
+      if s.bar.x == 0 then
+        local timed = rubato.timed {
+          duration = 1 / 6,
+          intro = 1 / 13,
+          override_dt = true,
+          subscribed = function(pos)
+            s.bar.x = pos
+          end
+        }
+        timed.target = 450
+      end
+    end)
 
-	awesome.connect_signal("fold::bar", function()
-		if s.bar2.x ~= 0 then
-			local timed = rubato.timed {
-				duration = 1 / 4,
-				intro = 1 / 9,
-				override_dt = true,
-				subscribed = function(pos)
-					s.bar.x = pos + 450
-				end
-			}
-			timed.target = -450
-			awful.spawn.easy_async('sleep 0.4', function()
-				s.bar.ontop = false
-			end)
-		end
-	end)
+    awesome.connect_signal("fold::bar", function()
+      if s.bar2.x ~= 0 then
+        local timed = rubato.timed {
+          duration = 1 / 4,
+          intro = 1 / 9,
+          override_dt = true,
+          subscribed = function(pos)
+            s.bar.x = pos + 450
+          end
+        }
+        timed.target = -450
+        awful.spawn.easy_async('sleep 0.4', function()
+          s.bar.ontop = false
+        end)
+      end
+    end)
 
-	vars.open = false
-	awesome.connect_signal("open::window", function()
-		vars.open = not vars.open
-		if vars.open then
-			s.bar.ontop = true
-			local timed = rubato.timed {
-				duration = 1 / 5,
-				intro = 1 / 11,
-				override_dt = true,
-				subscribed = function(pos)
-					s.bar.x = pos
-					s.bar2.x = pos - 450
-				end
-			}
-			timed.target = 450
-		else
-			local timed = rubato.timed {
-				duration = 1 / 4,
-				intro = 1 / 9,
-				override_dt = true,
-				subscribed = function(pos)
-					s.bar.x = pos + 450
-					s.bar2.x = pos
-				end
-			}
-			timed.target = -450
-			awful.spawn.easy_async("sleep 0.25", function()
-				s.bar.ontop = false
-			end)
-		end
-	end)
+    vars.open = false
+    awesome.connect_signal("open::window", function()
+      vars.open = not vars.open
+      if vars.open then
+        s.bar.ontop = true
+        local timed = rubato.timed {
+          duration = 1 / 5,
+          intro = 1 / 11,
+          override_dt = true,
+          subscribed = function(pos)
+            s.bar.x = pos
+            s.bar2.x = pos - 450
+          end
+        }
+        timed.target = 450
+      else
+        local timed = rubato.timed {
+          duration = 1 / 4,
+          intro = 1 / 9,
+          override_dt = true,
+          subscribed = function(pos)
+            s.bar.x = pos + 450
+            s.bar2.x = pos
+          end
+        }
+        timed.target = -450
+        awful.spawn.easy_async("sleep 0.25", function()
+          s.bar.ontop = false
+        end)
+      end
+    end)
+  end
 end)
--- awesome/signals/screen/init.lua
+++ awesome/signals/screen/init.lua
@@ -1,52 +1,50 @@
-local awful = require 'awful'
-local beautiful = require 'beautiful'
+local awful = require ("awful")
+local beautiful = require ("beautiful")
+local gears = require ("gears")
 
-local gears = require 'gears'
-
-local function set_wallpaper(s)
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
+-- Set wallpaper for a screen
+local function set_wallpaper (s)
+  if beautiful.wallpaper then
+    local wallpaper = type (beautiful.wallpaper) == "function"
+        and beautiful.wallpaper (s)
+      or beautiful.wallpaper
+    gears.wallpaper.maximized (wallpaper, s, true)
+  end
 end
 
-screen.connect_signal("property::geometry", set_wallpaper)
+-- Update wallpaper when screen geometry changes
+screen.connect_signal ("property::geometry", set_wallpaper)
 
-awful.screen.connect_for_each_screen(function(s)
-    set_wallpaper(s)
-end)
+-- Screen setup
+awful.screen.connect_for_each_screen (function (s)
+  set_wallpaper (s)
 
-screen.connect_signal('request::desktop_decoration', function(s)
-  awful.tag.add("1", {
-    layout             = awful.layout.suit.fair,
-    master_fill_policy = "expand",
-    screen             = s,
-    selected           = true,
-  })
-
-  awful.tag.add("2", {
-    layout             = awful.layout.suit.tile,
-    master_fill_policy = "expand",
-    screen = s,
-  })
-
-  awful.tag.add("3", {
-    layout             = awful.layout.suit.tile,
-    master_fill_policy = "expand",
-    master_width_factor = 0.65,
-    screen             = s,
-  })
-
-  awful.tag.add("4", {
-    layout             = awful.layout.suit.tile,
-    screen = s,
-  })
-
-  awful.tag.add("5", {
-    layout             = awful.layout.suit.tile,
-    screen = s,
-  })
+  -- Define tag layouts for different screens
+  local suit = awful.layout.suit
+  local tags_by_screen = {
+    [1] = {
+      { name = "1", layout = suit.fair, selected = true },
+      { name = "2", layout = suit.tile },
+      { name = "3", layout = suit.tile, master_width_factor = 0.65 },
+      { name = "4", layout = suit.tile },
+      { name = "5", layout = suit.tile },
+    },
+    [2] = {
+      { name = "6", layout = suit.tile },
+      { name = "7", layout = suit.tile },
+    },
+  }
+                             
+  -- Apply tags based on screen index
+  if tags_by_screen[s.index] then
+    for _, tag_def in ipairs (tags_by_screen[s.index]) do
+      awful.tag.add (tag_def.name, {
+        layout = tag_def.layout,
+        master_fill_policy = "expand",
+        master_width_factor = tag_def.master_width_factor,
+        screen = s,
+        selected = tag_def.selected or false,
+      })
+    end
+  end
 end)
